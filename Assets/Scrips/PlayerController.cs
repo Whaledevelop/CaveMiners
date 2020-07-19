b4d04d3cf7fd88804d6651bd27d4df73d96a21e4
Nikita Serebryakov <31417980+Whaledevelop@users.noreply.github.com>
2020-07-19 11:42:45 +0300

    Basic WASD movement with blocking by walls

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -72,38 +72,87 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         if (movement != Vector2.zero)
-        {
-            movementEndPoint = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
-            if (!CheckIfMovementBlocked(movementEndPoint))
+        {             
+            if (!CheckIfMovementBlocked())
+            {
+                movementEndPoint = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
                 rb.MovePosition(movementEndPoint);
+            }
+                
             movement = Vector2.zero;
         }
     }
 
-    private bool CheckIfMovementBlocked(Vector2 movementEndPoint)
+    public float blockingLayerOffset;
+
+    private bool CheckIfMovementBlocked()
     {
         if (blockingLayer == default)
+        {
             return false;
+        }            
         else
         {
+            if (movement.x != 0)
+            {
+                float xColliderOffset = movement.x * (boxCollider2D.offset.x + boxCollider2D.size.x / 2);
 
-            // Не хотим попасть в наш собственный коллайдер
-            boxCollider2D.enabled = false;
+                float yOffset = boxCollider2D.offset.y + boxCollider2D.size.y / 2;
 
-            // Луч пускаем не из rb.position, не из центра, а ближайшего к стороне движения края коллайдера
-            
-            float colliderXOffset = movement.x != 0 ? (movement.x > 0 ? boxCollider2D.size.x : -boxCollider2D.size.x) : 0;
-            float colliderYOffset = movement.y != 0 ? -boxCollider2D.size.y : 0; // И при движении вверх, и вниз, отступ считаем от ног
-            Vector2 colliderOffset = new Vector2(colliderXOffset, colliderYOffset);
+                bool bottomPointBlock = CheckIfOffsetBlocked(new Vector2(xColliderOffset, -yOffset), movement * 0.9f);
+                if (bottomPointBlock)
+                    return true;
+                else
+                {
+                    Vector2 offset = new Vector2(xColliderOffset, -yOffset) + movement * (blockingLayerOffset);
+                    bool offsetBottomBlock = CheckIfOffsetBlocked(offset, new Vector2(0, -0.9f));
+                    if (offsetBottomBlock)
+                        return true;
+                    else
+                        return CheckIfOffsetBlocked(offset, new Vector2(0, 0.9f));
+                }                    
+            }
+            else if (movement.y != 0)
+            {
+                float yColliderOffset = -(boxCollider2D.offset.y + boxCollider2D.size.y / 2); // И при движении вверх, и вниз, отступ считаем от ног
 
-            Debug.Log("rb.position : " + rb.position + ", offset : " + colliderOffset + ", with offset : " + (rb.position + colliderOffset));
-            // Проверяем, есть ли блокирующий слов на пути движения
-            RaycastHit2D hit = Physics2D.Linecast(rb.position + colliderOffset, movementEndPoint, blockingLayer);
+                float xOffset = boxCollider2D.offset.x + boxCollider2D.size.x / 2;
 
+                bool rightPointBlocked = CheckIfOffsetBlocked(new Vector2(xOffset, yColliderOffset), movement);
 
-            boxCollider2D.enabled = true;
-
-            return hit.transform != null;
+                if (rightPointBlocked)
+                    return true;
+                else
+                    return CheckIfOffsetBlocked(new Vector2(-xOffset, yColliderOffset), movement);
+            }
+            return false;
         }
+    }
+
+    private (Vector2, Vector2) gizmosPoints;
+
+    private bool CheckIfOffsetBlocked(Vector2 colliderOffset, Vector2 blockingLayerAxis)
+    {
+        Vector2 colliderEdgePoint = rb.position + colliderOffset;
+        Vector2 blockingLayerEdgePoint = colliderEdgePoint + blockingLayerAxis * blockingLayerOffset;
+
+        gizmosPoints = (blockingLayerEdgePoint, colliderEdgePoint);
+
+        // Не хотим попасть в наш собственный коллайдер
+        boxCollider2D.enabled = false;
+        // Проверяем, есть ли блокирующий слов на пути движения
+        RaycastHit2D hit = Physics2D.Linecast(colliderEdgePoint, blockingLayerEdgePoint, blockingLayer);
+
+        boxCollider2D.enabled = true;
+
+        return hit.transform != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(gizmosPoints.Item1, 0.05f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(gizmosPoints.Item2, 0.05f);
     }
 }
