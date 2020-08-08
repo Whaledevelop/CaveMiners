5a615fdc5b6c561fe762079ce4edf63e928de225
Nikita Serebryakov <31417980+Whaledevelop@users.noreply.github.com>
2020-08-08 22:55:58 +0300

    Start game with chosen characters

--- Assets/Scripts/Actors/MainActor.cs
+++ Assets/Scripts/Actors/MainActor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
 using UnityEngine.Rendering;
 
 public class MainActor : MonoBehaviour
@@ -10,9 +11,19 @@ public class MainActor : MonoBehaviour
 
     [SerializeField] private CharacterInitialDataSet chosenCharacters;
 
+    [SerializeField] private GameObject characterPrefab;
+
+    [SerializeField] private Transform charactersParent;
+
     public void Start()
     {
-        
+        for(int i = 0; i < chosenCharacters.Items.Count; i++)
+        {
+            GameObject characterObject = Instantiate(characterPrefab, charactersParent);
+            characterObject.transform.position = new Vector2(characterObject.transform.position.x + i * 0.1f, characterObject.transform.position.y);
+            SpriteResolver spriteResolver = characterObject.GetComponentInChildren<SpriteResolver>();
+            spriteResolver.SetCategoryAndLabel(chosenCharacters.Items[i].spriteName, spriteResolver.GetLabel());
+        }
     }
 
     public void OnChangeMoney(float money)
